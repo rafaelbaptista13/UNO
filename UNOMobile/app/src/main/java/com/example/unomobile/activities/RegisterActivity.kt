91c8001c68a51bc0458ae4fc2bc0227614d6d4ee
Rafael Baptista <rafaelbaptista@ua.pt>
2023-02-15 18:23:02 +0000

    Register users in Mobile App

--- UNOMobile/app/src/main/java/com/example/unomobile/activities/RegisterActivity.kt
+++ UNOMobile/app/src/main/java/com/example/unomobile/activities/RegisterActivity.kt
@@ -1,14 +1,17 @@
 package com.example.unomobile.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.lifecycleScope
 import com.example.unomobile.R
+import com.example.unomobile.models.ResponseMessage
+import com.example.unomobile.models.UserInfoToRegister
+import com.example.unomobile.network.Api
 import com.example.unomobile.validation.FormFieldText
 import com.example.unomobile.validation.disable
 import com.example.unomobile.validation.enable
@@ -16,12 +19,22 @@ import com.example.unomobile.validation.validate
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import reactivecircus.flowbinding.android.view.clicks
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class RegisterActivity : AppCompatActivity() {
 
     lateinit var login_button : AppCompatButton;
     lateinit var register_button : AppCompatButton;
+    private val messages : Map<String, String> = mapOf(
+        "User was registered successfully!" to "Utilizador registado com sucesso!",
+        "Failed! Email is already in use!" to "Erro! Email já está a ser utilizado!",
+        "User Not found." to "Utilizador não encontrado."
+    )
 
     private val fieldFirstName by lazy {
         FormFieldText(
@@ -130,16 +143,49 @@ class RegisterActivity : AppCompatActivity() {
         formFields.disable()
         if (formFields.validate()) {
 
-            // use data here
-            fieldFirstName.value
-            fieldLastName.value
-            fieldEmail.value
-            fieldPassword.value
+            val user_info = UserInfoToRegister(
+                first_name = fieldFirstName.value!!,
+                last_name = fieldLastName.value!!,
+                email = fieldEmail.value!!,
+                password = fieldPassword.value!!)
 
-            showToast("Conta criada com sucesso!")
+            Api.retrofitService.createAccount(user_info).enqueue(object: Callback<ResponseMessage> {
+                override fun onResponse(call: Call<ResponseMessage>, response: Response<ResponseMessage>) {
+                    Log.i("RegisterActivity", response.toString())
+                    if (response.isSuccessful) {
+                        Log.i("RegisterActivity", "Response was successful")
+                        val message = response.body()!!.message
+                        var translated_error_message = if (messages.containsKey(message)) {
+                            messages[message]
+                        } else {
+                            "Conta criada com sucesso."
+                        }
+                        showToast(translated_error_message!!)
 
-            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-            startActivity(intent)
+                        // Redirect to Login
+                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.i("RegisterActivity", "Response was unsuccessful")
+                        val error_body = JSONObject(response.errorBody()!!.string())
+                        val error_message = error_body.getString("message")
+                        var translated_error_message = if (messages.containsKey(error_message)) {
+                            messages[error_message]
+                        } else {
+                            "Ocorreu um erro ao criar a sua conta! Tente novamente mais tarde."
+                        }
+
+                        showToast(translated_error_message!!)
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseMessage>, t: Throwable) {
+                    t.printStackTrace()
+                    showToast("Ocorreu um erro ao criar a sua conta! Tente novamente mais tarde.")
+                }
+
+            })
+
         }
         formFields.enable()
 
@@ -150,3 +196,4 @@ class RegisterActivity : AppCompatActivity() {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
+
--- UNOMobile/app/src/main/java/com/example/unomobile/network/ApiService.kt
+++ UNOMobile/app/src/main/java/com/example/unomobile/network/ApiService.kt
@@ -2,12 +2,16 @@ package com.example.unomobile.network
 
 import com.example.unomobile.models.Activity
 import com.example.unomobile.models.Content
+import com.example.unomobile.models.ResponseMessage
+import com.example.unomobile.models.UserInfoToRegister
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 private const val BASE_URL = "http://10.0.2.2:8080/api/"
@@ -22,6 +26,9 @@ interface ApiService {
 
     @GET("activities")
     fun getActivities(@Query("weekcontent_id") weekcontent_id: String): Call<List<Activity>>
+
+    @POST("auth/signup")
+    fun createAccount(@Body user_data: UserInfoToRegister): Call<ResponseMessage>
 }
 
 object Api {
