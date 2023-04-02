d1784d3a78d2e5f1b3d8bc540bd2ce7a871522e6
Rafael Baptista <rafaelbaptista@ua.pt>
2023-04-02 23:44:29 +0100

    Updated docker-compose to build and use the images

--- unoweb/.next/static/chunks/webpack.js
+++ unoweb/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "52587393b071ac88"; }
+/******/ 		__webpack_require__.h = function() { return "1b9059f144f60dce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
