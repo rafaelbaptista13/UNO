cfb2895e417f1bf3be51d54e65a10a36193313e8
Rafael Baptista <rafaelbaptista@ua.pt>
2023-04-18 17:04:45 +0100

    Updated webapp to perform requests correctly to nginx

--- unoweb/next.config.js
+++ unoweb/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  basePath: process.env.NODE_ENV === "production" ? '/rb-md-violuno-app-v1' : "",
 }
 
 module.exports = nextConfig
